bde5c9b97d8e0306f8ba857849c84be400c3ba33
felipe Sampaio <samlipe2006@gmail.com>
2021-07-09 21:53:44 -0300

    desafio 10 feito 09/07/2021

---
+++ src/desafio10exer1respostas/Desafio10EXER1respostaS.java
@@ -0,0 +1,13 @@
+package desafio10exer1respostas;
+/**
+ *
+ * @author Felipe
+ */
+public class Desafio10EXER1respostaS {
+    public static void main(String[] args) {
+        for(int cont=2;cont<=100;cont+=2){
+            System.out.println(cont);
+    }
+    }     
+    
+}
